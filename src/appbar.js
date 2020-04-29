c00ddaaf1aecf7ca642123fa613cbc225156d5f5
Riju Pramanik <pramanikriju@gmail.com>
2020-04-29 11:33:24 +0530

    Siebar drawer added

--- src/appbar.js
+++ src/appbar.js
@@ -17,6 +17,8 @@ import clsx from "clsx";
 import Paper from "@material-ui/core/Paper";
 import { Grid } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import CommuteIcon from "@material-ui/icons/Commute";
 
 const useStyles = makeStyles((theme) => ({
   centered: {
@@ -49,11 +51,18 @@ const useStyles = makeStyles((theme) => ({
     color: "white",
     margin: 10,
   },
+  user: {
+    margin: 10,
+  },
 }));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
 
 export default function Appbar() {
   const classes = useStyles();
-  const [drawer, setDrawer] = useState(false);
+  const [drawer, setDrawer] = useState(true);
 
   const toggleDrawer = () => (event) => {
     if (
@@ -90,9 +99,9 @@ export default function Appbar() {
           <Grid
             container
             spacing={3}
-            align="center"
-            justify="center"
-            direction="column"
+            alignItems="center"
+            justify="flex-start"
+            direction="row"
             className={classes.avatar}
           >
             <Grid item xs={2}>
@@ -105,8 +114,58 @@ export default function Appbar() {
                 RP
               </Avatar>
             </Grid>
+            <Grid
+              xs="8"
+              align="center"
+              justify="center"
+              className={classes.user}
+            >
+              <Typography variant="h6" component="h6">
+                User Name
+              </Typography>
+            </Grid>
           </Grid>
         </Paper>
+        <Grid
+          container
+          spacing={3}
+          align="center"
+          justify="center"
+          direction="column"
+          //className={classes.avatar}
+        >
+          <Grid item xs={12}>
+            <List component="nav" aria-label="main mailbox folders">
+              <ListItem button>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="New Bookings" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Booking history" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <CommuteIcon />
+                </ListItemIcon>
+                <ListItemText primary="Vehicles" />
+              </ListItem>
+            </List>
+            <Divider />
+            <List component="nav" aria-label="secondary mailbox folders">
+              <ListItem button>
+                <ListItemText primary="Trash" />
+              </ListItem>
+              <ListItemLink href="#simple-list">
+                <ListItemText primary="Logout" />
+              </ListItemLink>
+            </List>
+          </Grid>
+        </Grid>
       </Drawer>
     </div>
   );
